62e0e38c41e1a11481981568b00208d5216ce66a
Muhammad Rayhan Novelo <rayhan.novelo@gmail.com>
2024-10-21 10:24:26 +0700

    rename app name

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -72,7 +72,7 @@ export default function RootLayout() {
           <Stack.Screen
             name="index"
             options={{
-              title: "2FA PPI AUTHENTICATOR",
+              title: "PPI AUTHENTICATOR",
               headerRight: () => <ThemeToggle />,
             }}
           />
